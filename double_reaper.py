6fbeb0cc020ed06d07c924645eaf510990e8d3cc
Glenn Benedict Montesclaros <84988544+montesclarosglennbenedict@users.noreply.github.com>
2024-05-08 14:06:19 -0700

    dump

---
+++ double_reaper.py
@@ -0,0 +1,38 @@
+
+import os
+import ast
+import json
+
+folder_path = './ramen'
+
+# Ensure the folder path is valid
+if not os.path.exists(folder_path):
+    print(f"The folder path '{folder_path}' does not exist.")
+else:
+    # Loop through all files in the folder
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        
+        # Check if the file is a JSON file
+        if file_path.endswith('.json'):
+            try:
+                # Open and load the JSON file
+                with open(file_path, 'r') as json_file:
+                    data = json.load(json_file)
+                    
+                    # Check if the "articulations" key exists in the JSON
+                    try:
+                        articulations_value = data['result']['articulations']
+                        try:
+                            output_file_path = "./ramen_dump/"+filename
+                            with open(output_file_path, 'w') as file:
+                                file.write(articulations_value)
+                        except:
+                            with open(output_file_path, 'w') as file:
+                                file.write("None")
+                    except:
+                        print('bleh')
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON in file '{filename}': {e}")
+            except Exception as e:
+                print(f"Error processing file '{filename}': {e}")
--- reaper.py
+++ reaper.py
@@ -7,11 +7,13 @@ for institution in institutions:
     institution_id = str(institution['id'])
     institution_agreements = assistorgapi.get_institutions_agreements(institution_id)
     agreement = json.loads(assistorgapi.get_agreements("117", institution_id, "74", 'major'))
-    desired_label = "Data Theory/B.S."
+    desired_label = "Computer Science and Engineering/B.S."
     desired_key = str(next((report["key"] for report in agreement["reports"] if report["label"] == desired_label), None))
     agreement_mod = assistorgapi.get_agreements_mod(desired_key)
-    print(desired_key)
-    print(agreement_mod)
-    output_file_path = "./data_theory_ucla/"+institution_id+".json"
-    with open(output_file_path, 'w') as json_file:
-        json.dump(agreement_mod, json_file, indent=2)
+    output_file_path = "./ramen/"+institution_id+".json"
+    try:
+        with open(output_file_path, 'w') as file:
+            file.write(agreement_mod)
+    except:
+        with open(output_file_path, 'w') as file:
+            file.write("None")
